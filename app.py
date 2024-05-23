51315031f6fe4b279d323c7e1be283ac611133c3
takedaSouda <2394822700@qq.com>
2024-05-23 21:55:20 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def forge():
         db.session.add(movie)
     db.session.commit()
     click.echo( 'Done.')
-    
+
 
 
 @app.route('/home')
@@ -76,9 +76,21 @@ movies = [
     ]
 name='miao'
 
-@app.route('/')
-     
-def index():
-    return render_template('index.html',username=name,movie=movies)
 
-print('hellow')
+
+@app.context_processor
+def inject_user():
+    user=User.query.first()
+    return dict(user=user)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.route('/')
+def index():
+    movies=Movie.query.all()
+    return render_template('index.html',username=name,movies=movies)
+
+
