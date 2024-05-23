93fecf72bfe968a38c2b5710f052ce3942cb19d1
takedaSouda <2394822700@qq.com>
2024-05-23 10:06:30 +0800

    add index page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,url_for
+from flask import Flask,url_for,render_template
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -16,5 +16,22 @@ def test():
     print(url_for('hello_world'))
     q=url_for('userpage',username='miao')
     return f'test page{q}'
+
+
+movies = [
+    {'name':'The Shawshank Redemption','year':'1994'},
+    {'name':'The Godfather','year':'1972'},
+    {'name':'The Dark Knight','year':'2008'},
+    {'name':'12 Angry Men','year':'1957'},
+    {'name':'Schindlers List','year':'1993'},
+    {'name':'Pulp Fiction','year':'1994'},
+    {'name':'The Lord of the Rings: The Return of the King','year':'1999'}
+    ]
+name='miao'
+
+@app.route('/')
+     
+def index():
+    return render_template('index.html',username=name,movie=movies)
 
 print('hellow')
