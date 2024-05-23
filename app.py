110898f1aeb6e0ebca9f05c8fefde632b5aaea1e
takedaSouda <2394822700@qq.com>
2024-05-23 13:09:55 +0800

    Add data support with Flask-SQLalchemy

--- app.py
+++ app.py
@@ -1,7 +1,54 @@
 from flask import Flask,url_for,render_template
 from markupsafe import escape
+from flask_sqlalchemy import SQLAlchemy
+import os
+import click
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(app.root_path,'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+db=SQLAlchemy(app)
+
+class User(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(20))
+
+class Movie(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(60))
+    year=db.Column(db.String(4))
+
+
+@app.cli.command()
+@click.option('--drop',is_flag=True,help='Create after drop.')
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')
+
+@app.cli.command()
+def forge():
+    db.create_all()
+    name='greg li'
+    movies=[
+        {'title':'My Neighbor Totoro','year':'1988'},
+        {'title':'Dead Poets Society','year':'1989'},
+        {'title':'A Perfect World','year':'1993'},
+        {'title':'Leon','year':'1994'},
+        {'title':'Mahjong','year':'1996'},
+        {'title':'Swallowtail Butterfly','year':'1996'},
+        ]
+    user=User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie=Movie(title=m['title'],year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo( 'Done.')
+    
+
 
 @app.route('/home')
 def hello_world():
